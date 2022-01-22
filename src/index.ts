e6bb02ec9df0564c8fd3bfa277990e3fced8dbf7
hamishgibbs <hamishgibbs@gmail.com>
2022-01-22 20:50:05 +0000

    layout core objects

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,32 @@ export const Greeter = (name: string) => `Hello ${name}`;
 export const Node = (title: string, type: string) => {
   return title
 }
+
+
+/*
+Node {
+	uuid: 1
+	created: ts
+	removed: ts
+}
+
+Edge {
+	subject: uuid
+	predicate: string
+	object: uuid
+	qualifiers: [string: uuid, ...]
+	created: ts
+	removed: ts
+}
+
+Graph {
+  nodes: []
+  edges: []
+}
+
+History: {
+  nodes: []
+  edges: []
+}
+
+*/
