1a06903a8f1e7f6515be7feaa68ce394ef68d988
hamishgibbs <hamishgibbs@gmail.com>
2022-01-23 11:34:24 +0000

    clean up

--- src/__tests__/Universe.test.ts
+++ src/__tests__/Universe.test.ts
@@ -10,30 +10,27 @@ test('Universe Creation', () => {
 test('Universe.addNode()', () => {
   const universe = new Universe();
   expect(universe.addNode()).toEqual(expect.not.stringMatching(uuid_re));
-  expect(universe.graph.nodes).toHaveLength(1)
+  expect(universe.graph.nodes).toHaveLength(1);
 });
 
 test('Universe.addEdge()', () => {
   const universe = new Universe();
-  const res = universe.addEdge(
-    "this", "isa", "that",
-    [{qualifier: "on", target: "here"}]
-  )
+  const res = universe.addEdge('this', 'isa', 'that', [{ qualifier: 'on', target: 'here' }]);
   expect(res).toEqual(expect.not.stringMatching(uuid_re));
-  expect(universe.graph.edges).toHaveLength(1)
+  expect(universe.graph.edges).toHaveLength(1);
 });
 
 test('Universe.toYAML()', () => {
   const universe = new Universe();
   const res = universe.toYAML();
-  const expected = "graph:\n  nodes: []\n  edges: []\nhistory:\n  nodes: []\n  edges: []\n"
-  expect(res).toBe(expected)
-})
+  const expected = 'graph:\n  nodes: []\n  edges: []\nhistory:\n  nodes: []\n  edges: []\n';
+  expect(res).toBe(expected);
+});
 
 test('Universe.fromYAML()', () => {
   const universe = new Universe();
-  const document = "graph:\n  nodes: []\n  edges: []\nhistory:\n  nodes: []\n  edges: []\n"
+  const document = 'graph:\n  nodes: []\n  edges: []\nhistory:\n  nodes: []\n  edges: []\n';
   universe.fromYAML(document);
-  expect(universe.graph).toStrictEqual({edges: [], nodes: []})
-  expect(universe.history).toStrictEqual({edges: [], nodes: []})
-})
+  expect(universe.graph).toStrictEqual({ edges: [], nodes: [] });
+  expect(universe.history).toStrictEqual({ edges: [], nodes: [] });
+});
--- src/universe.ts
+++ src/universe.ts
@@ -1,7 +1,7 @@
 import { Node } from './node';
 import { Edge, IQualifier } from './edge';
 import * as yaml from 'js-yaml';
-// import {fs} from 'fs';
+import { writeFileSync, readFileSync } from 'fs';
 
 interface IGraph {
   nodes: Node[];
@@ -28,42 +28,34 @@ export class Universe {
     return node.id;
   }
 
-  addEdge(
-    subject: string,
-    predicate: string,
-    object: string,
-    qualifiers: IQualifier[] = []) {
-    const edge = new Edge(
-      subject,
-      predicate,
-      object,
-      qualifiers
-    );
+  addEdge(subject: string, predicate: string, object: string, qualifiers: IQualifier[] = []) {
+    const edge = new Edge(subject, predicate, object, qualifiers);
     this.graph.edges.push(edge);
     return edge.id;
   }
 
   toYAML() {
-    return yaml.dump({graph: this.graph, history: this.history})
+    return yaml.dump({ graph: this.graph, history: this.history });
   }
 
-  fromYAML(document: string) {
-    const data = yaml.load(document) as IUniverse
-    this.graph = data.graph
-    this.history = data.history
+  save(fn: string) {
+    writeFileSync(fn, this.toYAML());
   }
 
-  /*
+  fromYAML(doc: string) {
+    const data = yaml.load(doc) as IUniverse;
+    this.graph = data.graph;
+    this.history = data.history;
+  }
+
   load(fn: string) {
-    doc = yaml.load(fs.readFileSync(fn, 'utf8'))
-    // I / O needs to be separated here - this should deal with putting graph inplace and test that it is json
+    const doc = readFileSync(fn, 'utf8');
+    this.fromYAML(doc);
   }
-  */
 
-  // universe.toYML() - this can be unit tested
+  // remove entity
+  // remove node
+  // remove edge
 
-  // save() to save entire graph (to a file for now) - this can't be unit tested
-
-  // load () to construct entire graph (from a file for now) - this can't be unit tested
-
+  // building & versioning
 }
