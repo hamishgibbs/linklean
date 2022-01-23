259f3b356a4936e4081d60ec517e0b6d7606f63d
hamishgibbs <hamishgibbs@gmail.com>
2022-01-23 09:58:31 +0000

    add test fixtures

--- src/__tests__/Universe.test.ts
+++ src/__tests__/Universe.test.ts
@@ -1,3 +1,4 @@
+import { uuid_re } from './fixtures/utils'
 import { Universe } from '../index';
 
 test('Universe Creation', () => {
@@ -8,7 +9,6 @@ test('Universe Creation', () => {
 
 
 test('Universe.addNode()', () => {
-  const uuid_re = '/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i';
   var universe = new Universe();
   expect(universe.addNode()).toEqual(expect.not.stringMatching(uuid_re));
 });
